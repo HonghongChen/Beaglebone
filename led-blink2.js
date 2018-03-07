e7622aca6698b4f4ad5cca14f4479a9701edda36
HonghongChen <hong.hongchen@gmail.com>
2018-03-07 17:33:43 +0800

    add led-blkin2.js

---
+++ led-blink2.js
@@ -0,0 +1,13 @@
+var Gpio = require('onoff').Gpio;
+var led = Gpio(115, 'out');
+var interval;
+
+interval = setInterval(function(){
+    led.writeSync(led.readSync ^ 1);
+}, 200);
+
+setTimeout(function(){
+    clearInterval(interval);
+    led.writeSync(0);
+    led.unexport();
+},10000);
